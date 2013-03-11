f95547034eda2ead4930989394a08b5ffe79a9d0
david <david.opresnik12@gmail.com>
2013-03-11 10:07:41 +0100

    Show Files on the Home Page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharebox::Application.config.secret_token = '13c52f8d2ceb2fe5218f5b8990039b0a1e8cc9591642e2fe1c2993fa1ff3fa435a4254072335a9eb023e61e7db309f6c24e125f17d52f0f5561e81369873741d'
